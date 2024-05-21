adf99c11413ed63871d8b94a616b6f35b38a960b
atifnewcastle <atiframlan@gmail.com>
2024-05-21 15:25:55 +0800

    first commit

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap basic 101</title>
+    <link rel="stylesheet" href="./dist/css/bootstrap.min.css" />
+</head>
+<body>
+<!-- basic navigation-->
+<h1>welcome to bootstrap!</h1>
+<script src="./dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
