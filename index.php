8762623a3b1009d284d28b2221c5a5f3361527d4
fabianHCDCS270 <2023622802@student.uitm.edu.my>
2024-05-24 21:50:58 +0800

    added alignments styling classes added background color with appropriate text color

--- index.php
+++ index.php
@@ -6,60 +6,79 @@
     <title>Bootstrap basic 101</title>
     <link rel="stylesheet" href="./dist/css/bootstrap.min.css" />
     <style>
+        .col-sm-1 {}
+        .col-sm-2 {}
+        .col-sm-3 {}
+        .col-sm-4 {}
+        .col-sm-5 {}
+        .col-sm-6 {}
+        .col-sm-7 {}
+        .col-sm-8 {}
+        .col-sm-9 {}
+        .col-sm-10 {}
+        .col-sm-11 {}
+        .col-sm-12 {}
+        .row{}
+        .container-fluid {}
+
+        .centerContent-byHeight{
+            display: flex;
+            justify-content: center;}
+        .centerContent-byWidth{
+            display: flex;
+            align-items: center;}
+        .centerContent-byBoth{
+            display: flex;
+            justify-content: center;
+            align-items: center;}
         .profile_picture {
             max-width: 90%;
             max-height: 90%;
             border-radius: 50%;
             border-width: 5px;
             border-style: solid;
-            border-color: white;
-        }
+            border-color: white;}
         .mainHeader_L, .mainParagraph_L {
             padding: 3px;
-            text-align:Left;
-        }
+            text-align:Left;}
         .breaker1 {
             border: 5px solid white;
-            width: 50%;
-        }
-        .container-fluid {
-            padding: 20px 0px 0px 0px;
-            color: white;
-            text-align: center;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-        }
+            width: 79.2%;} 
+        .addShadow{
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);}
         .size3_card {
+            padding: 4px 0px 0px 10px;
             background-color: white;
             color: black;
             border-radius: 10px;
             margin: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-        }
-        .col-sm-4 {
-            
-        }
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);}
+        .bg-black100{
+            background-color: black;
+            color: white;}
+        .pad10{
+            padding: 10px 10px 10px 10px;}
+        
     </style>
 
 </head>
 <body>
-<div class="container-fluid" style="background-color: black;">
-<div class="row">
-    <div class="col-sm-3" >
+<div class="container-fluid" style="background-color:black;">
+<div class="row centerContent-byBoth">
+    <div class="col-sm-3 centerContent-byBoth">
         <img src="./img/gambarFormal_2.jpg" class="profile_picture" alt=""> 
     </div>
-    <div class="col-sm-9">
+    <div class="col-sm-9 bg-black100 pad10">
         <div class="row">
             <div class="col">
                 <h1 class="mainHeader_L">My First Bootstrap Page</h1>
                 <hr class="breaker1">
                 <h2 class="mainHeader_L">
                     Fabian Harrison Anak Thomas Tarang 
-                    <br>(2023622802)
-                </h2>
+                    <br>(2023622802)</h2>
                 <p class="mainHeader_L">
-                    Bachelor in Mobile Computing, CDCS270 
-                    <br>Kampus Kuala Terengganu, Cendering
-                </p>
+                    Bachelor in Mobile Computing, CDCS270 <br>
+                    Kampus Kuala Terengganu, Cendering</p>
             </div>
         </div>
         <div class="row">
