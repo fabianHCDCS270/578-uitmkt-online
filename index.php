848e06b0a9f96e7af51b1ae004536cbb08a4bc26
fabianHCDCS270 <2023622802@student.uitm.edu.my>
2024-05-24 21:12:34 +0800

    Added style on some reserved classes Added shadow on main banner

--- index.php
+++ index.php
@@ -12,32 +12,67 @@
             border-radius: 50%;
             border-width: 5px;
             border-style: solid;
-            border-color: black;
+            border-color: white;
         }
         .mainHeader_L, .mainParagraph_L {
             padding: 3px;
             text-align:Left;
         }
+        .breaker1 {
+            border: 5px solid white;
+            width: 50%;
+        }
+        .container-fluid {
+            padding: 20px 0px 0px 0px;
+            color: white;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
+        .size3_card {
+            background-color: white;
+            color: black;
+            border-radius: 10px;
+            margin: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
+        .col-sm-4 {
+            
+        }
     </style>
 
 </head>
 <body>
-<div class="container-fluid p-5 bg-primary text-white text-center">
+<div class="container-fluid" style="background-color: black;">
 <div class="row">
-    <div class="col-sm-4" >
+    <div class="col-sm-3" >
         <img src="./img/gambarFormal_2.jpg" class="profile_picture" alt=""> 
     </div>
-    <div class="col-sm-8">
-        <h1 class="mainHeader_L">My First Bootstrap Page</h1>
-        <hr style="color:red; stroke: 2px;">
-        <h2 class="mainHeader_L">
-            Fabian Harrison Anak Thomas Tarang
-            2023622802
-        </h2>
-        <p class="mainHeader_L">
-            Contact: 011-2516 9289 <br>
-            Email: 2023622802@uitm.edu.my
-        </p>
+    <div class="col-sm-9">
+        <div class="row">
+            <div class="col">
+                <h1 class="mainHeader_L">My First Bootstrap Page</h1>
+                <hr class="breaker1">
+                <h2 class="mainHeader_L">
+                    Fabian Harrison Anak Thomas Tarang 
+                    <br>(2023622802)
+                </h2>
+                <p class="mainHeader_L">
+                    Bachelor in Mobile Computing, CDCS270 
+                    <br>Kampus Kuala Terengganu, Cendering
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3 size3_card">
+                <h3 class="mainHeader_L"> Test1</h3>
+            </div>
+            <div class="col-sm-3 size3_card">
+                <h3 class="mainHeader_L"> Test2</h3>
+            </div>
+            <div class="col-sm-3 size3_card">
+                <h3 class="mainHeader_L"> Test3</h3>
+            </div>
+        </div>
     </div>
 </div>
     
